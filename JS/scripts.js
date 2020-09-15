430c481049916087c69b16a2bfee161148fcb917
KiR5267 <61653610+KiR5267@users.noreply.github.com>
2020-09-15 18:41:17 +0300

    Add files via upload

---
+++ JS/scripts.js
@@ -0,0 +1,69 @@
+console.log('helloy');
+
+
+let a1 = 5 % 3;
+console.log('a1');
+let a2 = 3 % 5;
+console.log('a2');
+let a3 = 5 + '3';
+console.log('a3');
+//let a4 = 5– 3;
+//console.log(a4);
+let a5 = 75 + 'кг';
+console.log('a5');
+let a6 = 785 * 653;
+console.log('a6');
+let a7 = 100 / 25;
+console.log('a7');
+let a8 = 0 * 0;
+console.log('a8');
+let a9 = 0 / 2;
+console.log('a9');
+let a10 = 89 / 0;
+console.log('a10');
+let a11 = 98 + 2;
+console.log('a11');
+//let a12 = 5 – 98;
+//console.log(a12);
+let a13 = (8 + 56 * 4) / 5;
+console.log('a13');
+let a14 = (9 - 12) * 7 / (5 + 2);
+console.log('a14');
+let a15 = +"123";
+console.log('a15');
+let a16 = 1 || 0;
+console.log('a16');
+let a17 = false || true;
+console.log('a17');
+let a18 = true > 0;
+console.log('a18');
+
+
+let t1 = ('В ту же минуту безо всяких туч');
+console.log(t1);
+let t2 = ('блеснула молния и ударил гром');
+console.log(t2);
+let t3 = ('индо земля зашаталась под ногами-и вырос');
+console.log(t3);
+let t4 = ('как будто из-под земли, перед купцом');
+console.log(t4);
+let t5 = ('зверь не зверь, человек не человек');
+console.log(t5);
+let t6 = ('а так какое-то чудище, страшное и мохнатое');
+console.log(t6);
+let t7 = ('и заревел он голосом диким');
+console.log(t7);
+let t8 = ('Он подошёл и сорвал аленький цветочек');
+console.log(t8);
+
+
+let str = 123;
+console.log(str, typeof str);
+let sum = 15.8;
+console.log(sum, typeof sum);
+let num = 'true';
+console.log(num, typeof num);
+let flag = "true";
+console.log(flag, typeof flag);
+let txt = "Привет";
+console.log(txt, typeof txt);
